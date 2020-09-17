bb2552446771d3e182ac955e3aa41a99eeaab810
anharusp <nuro4ka2011@yandex.ru>
2020-09-17 16:47:19 +0300

    Deleted unknown references

--- src/main/java/com/acme/edu/message/ChatMessage.java
+++ src/main/java/com/acme/edu/message/ChatMessage.java
@@ -1,7 +1,5 @@
 package com.acme.edu.message;
 
-import com.acme.edu.server.command.CommandParserException;
-
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
