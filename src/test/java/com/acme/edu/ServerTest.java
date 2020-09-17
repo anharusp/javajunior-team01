ae7e8b0f7adf641cf25837150dcdce55271bf5b3
anharusp <nuro4ka2011@yandex.ru>
2020-09-17 12:30:43 +0300

    Updated initial test

--- src/test/java/com/acme/edu/ServerTest.java
+++ src/test/java/com/acme/edu/ServerTest.java
@@ -1,14 +1,28 @@
 package com.acme.edu;
 
 import com.acme.edu.server.Server;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.io.IOException;
 
-public class ServerTest {
+public class ServerTest implements SysoutCaptureAndAssertionAbility{
+    @Before
+    public void setUpSystemOut() throws IOException {
+        resetOut();
+        captureSysout();
+    }
+
+    @After
+    public void tearDown() {
+        resetOut();
+    }
+    //endregion
+
     @Test
     public void shouldRun() {
-        Server s = new Server();
-        assertTrue(true);
+        System.out.println("hi");
+        assertSysoutContains("hi");
     }
 }
