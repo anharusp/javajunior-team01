5de301f1ca32caf22930b098b2ad4c26d2a0f54e
Dasha <darya_gavrilova1999@mail.ru>
2020-09-17 12:20:38 +0300

    Added cycle where client send message to server.

--- src/main/java/com/acme/edu/client/Client.java
+++ src/main/java/com/acme/edu/client/Client.java
@@ -2,10 +2,16 @@ package com.acme.edu.client;
 
 import java.io.*;
 import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Client {
+    private static String message;
+    private static BufferedReader br;
+
     public static void main(String[] args) {
-        try (final Socket connection = new Socket("127.0.0.1", 10_000);
+
+        try (final Socket connection = new Socket("127.0.0.1", 10_000); //from args
              final DataInputStream input = new DataInputStream(
                      new BufferedInputStream(
                              connection.getInputStream()));
@@ -13,18 +19,20 @@ public class Client {
                      new BufferedOutputStream(
                              connection.getOutputStream()));
         ) {
+            br = new BufferedReader(new InputStreamReader(System.in));
 
-            System.out.println(connection.getLocalPort());
-            out.writeUTF("HW!!!"); //LOG INT 5
-            out.flush();
-            System.out.println(">> " + input.readUTF());
-
-            /*
-            zamestitel.log(5);
-            zamestitel.flush();
-            zamestitel.close();
-            */
-
+            while(connection.isConnected()) {
+                message = br.readLine();
+                if (CommandParser.isKnownCommand(message))
+                    out.writeUTF(decorateMessage(message));
+                else {
+                    System.out.println("Wrong Command! Try again");
+                    continue;
+                }
+                out.flush();
+                System.out.println(">> " + input.readUTF()); //server logging???
+            }
+            br.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
