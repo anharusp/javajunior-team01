87a2e61438fa7d45e75c9c54e1eddd61192a1b53
anharusp <nuro4ka2011@yandex.ru>
2020-09-17 12:06:58 +0300

    Added initial test

---
+++ src/test/java/com/acme/edu/ServerTest.java
@@ -0,0 +1,12 @@
+package com.acme.edu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ServerTest {
+    @Test
+    public void shouldRun() {
+        assertTrue(true);
+    }
+}
