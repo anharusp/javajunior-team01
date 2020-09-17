c6c60cca124f43b34cd174530260a80d34252b7f
hnblv <annbelyaeva@bk.ru>
2020-09-17 23:58:46 +0300

    fi jar problem

--- clientsStart.bat
+++ clientsStart.bat
@@ -1,4 +1,3 @@
 @echo off
-javac -sourcepath src/main/java -d target  src/main/java/com/acme/edu/client/Client.java
-java -jar target/Client.jar
-
+:: run from command line like: clientsStart.bat 'path to jar'
+java -cp %1;C:/Users/User/.m2/repository/com/google/code/gson/gson/2.8.6/gson-2.8.6.jar com.acme.edu.client.Client
