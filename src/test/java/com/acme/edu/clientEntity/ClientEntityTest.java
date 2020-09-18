332e478d9cab3c511b67307c78fcec41b22210b6
DashaGavr <darya_gavrilova1999@mail.ru>
2020-09-18 11:57:20 +0300

    New tests added:                 for ClientEntity                 for Logger                 for FileSaver

--- src/test/java/com/acme/edu/clientEntity/ClientEntityTest.java
+++ src/test/java/com/acme/edu/clientEntity/ClientEntityTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.mockito.Mockito.*;
 
-public class clientEntityTest {
+public class ClientEntityTest {
 
     @Test
     public void shouldGetNameWhenSmbAsk() {
