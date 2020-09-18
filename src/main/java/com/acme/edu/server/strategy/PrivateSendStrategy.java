7871e4b1a4942117a558721ee025f178f78a8fe0
Your Name <you@example.com>
2020-09-18 13:15:57 +0300

    dummy private send strategy

--- src/main/java/com/acme/edu/server/strategy/PrivateSendStrategy.java
+++ src/main/java/com/acme/edu/server/strategy/PrivateSendStrategy.java
@@ -4,14 +4,17 @@ import com.acme.edu.connection.NetConnection;
 import com.acme.edu.message.ChatMessage;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Set;
 
 public class PrivateSendStrategy implements Strategy{
+
     public PrivateSendStrategy(ChatMessage message) {
+
     }
 
     @Override
-    public void play(NetConnection clientConnection, Set<NetConnection> netConnectionSet) throws IOException, ClientExit {
-        
+    public void play(NetConnection clientConnection, Set<NetConnection> netConnectionSet, Map<String, NetConnection> nameToConnection) throws IOException, ClientExit {
+
     }
 }
